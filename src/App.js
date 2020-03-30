768d5bc5ce00d9d7a5695768caa6fddafff12fa6
badr tazi <tasibadr@gmail.com>
2020-03-30 10:43:14 +0100

    refactor: change the App component to a functional component with hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 // import * as BooksAPI from './BooksAPI'
 import "./App.css";
 import * as BooksAPI from "./BooksAPI";
@@ -6,76 +6,51 @@ import SearchScreen from "./SearchScreen";
 import HomeScreen from "./HomeScreen";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-class BooksApp extends React.Component {
-  state = {
-    showSearchPage: false,
-    books: []
-  };
+const BooksApp = props => {
+  const [books, updateBooks] = useState([]);
 
-  componentDidMount() {
-    this.getAllBooks();
-  }
-
-  getAllBooks() {
+  useEffect(() => {
     BooksAPI.getAll().then(books => {
-      this.setState(state => ({
-        ...state,
-        books
-      }));
-    });
-  }
+      updateBooks(books);
+    }, []);
+  });
 
-  handleBookUpdate = (book, shelf) => {
+  const handleBookUpdate = (book, shelf) => {
     BooksAPI.update(book, shelf).then(resp => {
       // after updating the book on the server add changes locally without reusing the api (getAll())
-      this.setState(state => {
-        let books = [];
-        if (book.shelf === "none") {
-          // if book isn't in collection add it
-          const newBook = { ...book, shelf };
-          books = [...state.books, newBook];
-        } else {
-          // if book is in collection update shelf only
-          books = state.books.map(b => {
-            if (b.id === book.id) {
-              return { ...b, shelf };
-            } else {
-              return b;
-            }
-          });
-        }
-        return {
-          ...state,
-          books
-        };
-      }, this.updateShelves);
+      let allBooks = [];
+      if (book.shelf === "none") {
+        // if book isn't in collection add it
+        const newBook = { ...book, shelf };
+        allBooks = [...books, newBook];
+      } else {
+        // if book is in collection update shelf only
+        allBooks = books.map(b => {
+          if (b.id === book.id) {
+            return { ...b, shelf };
+          } else {
+            return b;
+          }
+        });
+      }
+      updateBooks(allBooks);
     });
   };
 
-  render() {
-    const { books, shelves } = this.state;
-    return (
-      <div className="app">
-        <BrowserRouter>
-          <Switch>
-            <Route path="/search">
-              <SearchScreen
-                books={books}
-                onBookUpdate={this.handleBookUpdate}
-              />
-            </Route>
-            <Route exact path="/">
-              <HomeScreen
-                books={books}
-                shelves={shelves}
-                onBookUpdate={this.handleBookUpdate}
-              />
-            </Route>
-          </Switch>
-        </BrowserRouter>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/search">
+            <SearchScreen books={books} onBookUpdate={handleBookUpdate} />
+          </Route>
+          <Route exact path="/">
+            <HomeScreen books={books} onBookUpdate={handleBookUpdate} />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+};
 
 export default BooksApp;
